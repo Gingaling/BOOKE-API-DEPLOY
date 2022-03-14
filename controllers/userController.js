01b3d47ab5be10931490f7dcb2c70883c8e5e4ff
esin87 <esin.saribudak@gmail.com>
2022-03-14 16:38:31 -0500

    Add routes for Users

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,4 +13,49 @@ router.get('/', (req, res) => {
 	});
 });
 
+// CREATE: POST a new user
+// http://localhost:8000/api/users/
+router.post('/', (req, res) => {
+	// Grab the new user data out of the request body
+	const newUser = req.body;
+	// Use the User model to create a new instance of a User with that data
+	User.create(newUser).then((user) => {
+		// Send back the newly created user document as json data
+		res.json(user);
+	});
+});
+
+// YOU DO: SHOW an individual user
+// http://localhost:8000/api/users/id
+router.get('/:id', async (req, res) => {
+	// Async/await syntax
+	// try {
+	// 	const user = await User.findById(req.params.id);
+	// 	res.json(user);
+	// } catch (error) {
+	// 	console.log(error);
+	// }
+
+	// Promise-chaining syntax
+	User.findById(req.params.id)
+		.then((user) => res.json(user))
+		.catch(console.error);
+});
+
+// UPDATE: EDIT an individual user
+// http://localhost:8000/api/users/id
+router.put('/:id', async (req, res) => {
+	try {
+		const updatedUser = req.body;
+		const updatedDocument = await User.findByIdAndUpdate(
+			req.params.id,
+			updatedUser,
+			{ new: true }
+		);
+		res.json(updatedDocument);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
 module.exports = router;
